46ff9b39d1a1de07f8349595dd40adc4488e8222
ranjitha.duraisamy <ranjitha.duraisamy1@wipro.com>
2017-06-20 09:40:29 +0000

    comments are added

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,22 +1,27 @@
 var express = require('express');
 var router = express.Router();
-const request=require('request');
- 
+const request = require('request');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-   
-  res.render('index', { title: 'Express' });
+
+    res.render('index', {
+        title: 'Express'
+    });
 });
-router.get('/search',function(req,res){
-    request.get('https://api.themoviedb.org/3/search/movie?api_key=3d34e72c9badeb4e4254c09ec0109d8e&language=en-US&query='+req.query.name+'&page=1&include_adult=false',function(err,response,body){
-  
-   if(!err && response.statusCode === 200){
-       
-        res.send(response.body);
-        
-    }  else{
-        res.send('error occured in route');
-    }  
+
+/* get method for search */
+
+router.get('/search', function(req, res) {
+    request.get('https://api.themoviedb.org/3/search/movie?api_key=3d34e72c9badeb4e4254c09ec0109d8e&language=en-US&query=' + req.query.name + '&page=1&include_adult=false', function(err, response, body) {
+        /* if no error and status code = 200 , then the res data will be send */
+        if (!err && response.statusCode === 200) {
+
+            res.send(response.body);
+
+        } else {
+            res.send('error occured in route');
+        }
     });
 });
 module.exports = router;
--- server/test/test.spec.js
+++ server/test/test.spec.js
@@ -1,32 +1,39 @@
-let request=require('supertest');
+/* supertest and chai modules are required */
+let request = require('supertest');
 var should = require('chai').should();
-let app=require('../index');
-var url=request('http://localhost:3000');
-describe('Movie App testing',function(err){
-    it('Test for status code 200', function(done){
-       url
-       .get("/search?name=bahubali")
-       .expect(200)
-       .end(function(err,res){
-         if (err) {
-                        throw err;
-            }
-else{
-         res.ok.should.equal(true);
-     }
+let app = require('../index');
+var url = request('http://localhost:3000');
 
-         done();
-        });
-    });
-    it('test for type of json', function(){
+/* test code for movieApp */
+/* checking status code is 200 or not */
+
+describe('Movie App testing', function(err) {
+    it('Test for status code 200', function(done) {
         url
-        .get('/search?name=irumugan')
-        .expect(200)
-        .end(function(err,res){
-            var movieobj=JSON.parse(res);
-            expect(typeof(movieobj).to.deep.equal('object'));
-            done();
-        });
+            .get("/search?name=bahubali")
+            .expect(200)
+            .end(function(err, res) {
+                if (err) {
+                    throw err;
+                } else {
+                    res.ok.should.equal(true);
+                }
+
+                done();
+            });
     });
-    
+
+    /* trsting type of json */
+
+    it('test for type of json', function() {
+        url
+            .get('/search?name=irumugan')
+            .expect(200)
+            .end(function(err, res) {
+                var movieobj = JSON.parse(res);
+                expect(typeof(movieobj).to.deep.equal('object'));
+                done();
+            });
+    });
+
 });
